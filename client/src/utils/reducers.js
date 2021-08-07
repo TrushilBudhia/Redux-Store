3cc006f06b1c6b209571c0f7efe1e3e73d5a6f9a
Trushil Budhia <trushil.budhia@gmail.com>
2021-08-07 20:52:39 +0800

    Added starter code

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,101 @@
+import { useReducer } from 'react';
+import {
+  UPDATE_PRODUCTS,
+  ADD_TO_CART,
+  UPDATE_CART_QUANTITY,
+  REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  CLEAR_CART,
+  TOGGLE_CART,
+} from './actions';
+
+// TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
+export const reducer = (state, action) => {
+  switch (action.type) {
+    // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
+    // The state spread and updated products are returned
+    // The products property has a value of the action.products spread
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+    // TODO: Add a comment describing the functionality of the UPDATE_CART_QUANTITY case
+    // The cart property has a value of the new cart in the statement mapped with the products to be purchased and the quanitity
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
+    // TODO: Add a comment describing the functionality of the REMOVE_FROM_CART case
+    // The newState is returned as the value of the cart property
+    // newState has a value of the cart filtered without the products that don't have ids matching the items in the cart
+    // The cartOpen value is set as true if the newState length is greater than 0 i.e. if there are items in the cart
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
+
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+
+    // TODO: Add a comment describing what the default case is for
+    // If any of the cases aren't met, the default case will be used
+    default:
+      return state;
+  }
+};
+
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
