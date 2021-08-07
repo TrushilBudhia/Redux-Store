851d2406e4bd8721f0958c65f799d8940194e9d2
Trushil Budhia <trushil.budhia@gmail.com>
2021-08-07 22:07:14 +0800

    Updated application name, logos and manifest

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -44,7 +44,7 @@ function Nav() {
       <h1>
         <Link to="/">
           <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          Shop Plus You
         </Link>
       </h1>
 
